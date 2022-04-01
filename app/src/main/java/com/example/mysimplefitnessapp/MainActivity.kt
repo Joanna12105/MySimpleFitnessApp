9f6bea87d55527035a88d991f3c228750a7a0e1b
Joanna <joanna.tr48@gmail.com>
2022-04-01 10:59:00 +0200

    SimpleFitness App - Basisschritte - Daten abrufen

--- app/src/main/java/com/example/mysimplefitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/mysimplefitnessapp/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.mysimplefitnessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import splitties.toast.longToast
 import splitties.toast.toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    //Log-Variable und Konstante
+    private val TAG = "MainActivity"
+    private val mURL = "https://run.mocky.io/v3/e815e607-a1ba-4083-ae67-895c6078d4bb"
+
+    //Layout-Variable
+    private val btnLoad : Button by lazy{ findViewById(R.id.loadDataButton) }
+
+    //Variablen
+    private val mRequestQueue : RequestQueue by lazy { Volley.newRequestQueue(applicationContext) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +31,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val loadDataB = findViewById(R.id.loadDataButton) as Button
-
-        loadDataB.setOnClickListener {
-            toast("Button gedrückt")
+        btnLoad.setOnClickListener {
+            Log.i(TAG, "Button LadeDaten wurde gedrückt")
+            getDataFromInternet()
         }
+    }
 
+    private fun getDataFromInternet() {
+        val mStringRequest = StringRequest(
+            Request.Method.GET, mURL,
+            Response.Listener {
+                longToast(getString(R.string.success_response, it.toString()))
+            }, Response.ErrorListener {
+                Log.i(TAG, getString(R.string.error_response, it.toString()))
+            })
+
+        mRequestQueue.add(mStringRequest)
     }
 }
