6559f2fa94a6a532467bd99d0e035ae045a36f04
Joanna <joanna.tr48@gmail.com>
2022-04-07 19:51:08 +0200

    SimpleFitness App - Darstellungsoptionen (Optional) - Fehler implementation 'com.jjoe64:graphview:4.2.2'

--- app/src/main/java/com/example/mysimplefitnessapp/GraphDataActivity.kt
+++ app/src/main/java/com/example/mysimplefitnessapp/GraphDataActivity.kt
@@ -2,8 +2,11 @@ package com.example.mysimplefitnessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.bind
 import android.util.Log
 import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter
+import com.jjoe64.graphview.series.BarGraphSeries
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import org.json.JSONException
@@ -13,38 +16,70 @@ import splitties.alertdialog.okButton
 import java.io.BufferedReader
 import java.io.FileInputStream
 import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.Comparator
 
 class GraphDataActivity : AppCompatActivity() {
-
     //Log und Konstanten
     private val TAG = "GraphDataActivity"
     private val DATA_FILENAME = "FitnessData"
-
     //Layout
-    private val graph : GraphView by lazy{ findViewById(R.id.graph) }
-
+    private val graph : GraphView by lazy { findViewById(R.id.graph) }
     //Variablen
-    private var pulseSeries = LineGraphSeries<DataPoint>()
+    private var pulseSeries = BarGraphSeries<DataPoint>()
     private var lastXvalue : Double = 0.0
+    private var dataArray = arrayListOf<String>()
+    private val dataPoints : Array<DataPoint> by lazy { Array(dataArray.size, { DataPoint(0.0, 0.0) }) }
+    private var last : Date = Calendar.getInstance().time
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graph_data)
+
         getDataFromFile()
+        parseJSONToSeries()
+        configGraph()
+        pulseSeries = BarGraphSeries(dataPoints)
+        graph.addSeries(pulseSeries)
+    }
+
+    private fun configGraph() {
+        //y-Achse
+        graph.viewport.isYAxisBoundsManual = true
+        graph.viewport.setMinY(60.0)
+        graph.viewport.setMaxY(150.0)
+
+        //x-Achse
+        graph.viewport.isXAxisBoundsManual = true
+        graph.viewport.setMinX((last.time - 60*1000).toDouble())
+        graph.viewport.setMaxX((last.time).toDouble())
+
+        //enable scrolling and scaling
+        graph.viewport.isScalable = true
+        graph.viewport.setScalableY(true)
+
+        //Datumformat
+        val df = SimpleDateFormat("E, HH:mm:ss")
+        graph.gridLabelRenderer.labelFormatter = DateAsXAxisLabelFormatter(this, df)
+        graph.gridLabelRenderer.numHorizontalLabels = 2
+        graph.gridLabelRenderer.numVerticalLabels = 5
+        graph.gridLabelRenderer.padding = 50
+        graph.gridLabelRenderer.setHumanRounding(false)
+        graph.title = getString(R.string.graph_title)
     }
 
     private fun getDataFromFile() {
-        var fileInput : FileInputStream? = null
+        var fileInput : FileInputStream ? = null
         try {
             fileInput = openFileInput(DATA_FILENAME)
             val bufferdIn = BufferedReader(InputStreamReader(fileInput))
             var data = bufferdIn.readLine()
             while(data != null) {
-                parseJSON(data)
+                dataArray.add(data)
                 data = bufferdIn.readLine()
             }
             bufferdIn.close()
-            graph.addSeries(pulseSeries)
         }
         catch (e : Exception) {
             e.printStackTrace()
@@ -52,23 +87,48 @@ class GraphDataActivity : AppCompatActivity() {
         }
     }
 
-    private fun parseJSON(jsonString : String) {
-        try {
-            val obj = JSONObject(jsonString)
-            val pulse = obj.getInt("puls").toDouble()
-            Log.i(TAG, "Puls: $pulse")
+    private fun parseJSONToSeries() {
+        for(i in 0 until dataArray.size) {
+            try {
+                val jsonString = dataArray.get(i)
+                val obj = JSONObject(jsonString)
+                val pulse : Double = obj.getInt("puls").toDouble()
 
-            pulseSeries.appendData(DataPoint(lastXvalue, pulse), true, 1000)
-            lastXvalue++
+                val time = obj.getJSONArray("time")
+                val year = time.getInt(0)
+                val month = time.getInt(1)
+                val day = time.getInt(2)
+                val hour = time.getInt(3)
+                val minute = time.getInt(4)
+                val second = time.getInt(5)
+
+                val calendar = Calendar.getInstance()
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, day)
+                calendar.set(Calendar.HOUR_OF_DAY, hour)
+                calendar.set(Calendar.MINUTE, minute)
+                calendar.set(Calendar.SECOND, second)
+
+                last = calendar.time
+                dataPoints[i] =  DataPoint(last, pulse)
+            }
+            catch (e : JSONException) {
+                e.printStackTrace()
+                dialogError(e.localizedMessage)
+            }
         }
-        catch (e : JSONException) {
-            Log.e(TAG, "Error JSON Parsing")
-            e.printStackTrace()
-            dialogError(getString(R.string.error_json_parsing))
-        }
+        Arrays.sort(dataPoints, object : Comparator<DataPoint?> {
+            override fun compare(p0: DataPoint?, p1: DataPoint?): Int {
+                if (p1!!.x > p0!!.x) return -1
+                return if (p1.x < p0.x) 1 else 0
+            }
+        })
     }
+
     private fun dialogError(message : String) {
-        alertDialog(title = getString(R.string.error_msg_title), message = message) {
+        alertDialog(title = getString(R.string.error_msg_title),
+            message = message) {
             okButton{
                 finish()
             }
