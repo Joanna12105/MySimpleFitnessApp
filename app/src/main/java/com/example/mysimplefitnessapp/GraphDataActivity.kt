1d00c8181e6386898db45ae3713932815f1fbc9a
Joanna <joanna.tr48@gmail.com>
2022-04-07 17:21:52 +0200

    SimpleFitness App - GraphDataActivity eingefügt

---
+++ app/src/main/java/com/example/mysimplefitnessapp/GraphDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mysimplefitnessapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GraphDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graph_data)
+    }
+}
